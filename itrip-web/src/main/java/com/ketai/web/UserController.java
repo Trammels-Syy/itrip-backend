09f642c6d1a499ed3d2e8eec2fd1d5f8df1f7c76
帅帅的刘 <229010960@qq.com>
2021-12-17 12:12:40 +0800

    用户登录模块

---
+++ itrip-web/src/main/java/com/ketai/web/UserController.java
@@ -0,0 +1,17 @@
+package com.ketai.web;
+
+/**
+ * @Author:长沙科泰梦想学院
+ * @Description:
+ * @Date:Create In 2021/12/17 12:10
+ **/
+
+
+public class UserController {
+
+    public   String  userLogin(String  name ,String password){
+
+
+        return "SUCCESS";
+    }
+}
